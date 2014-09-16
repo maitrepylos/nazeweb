133c2a30ff044489a4fce38739707ca5bba372fe
cephee <joffreybody@hotmail.com>
2014-09-16 20:36:15 +0200

    commit joffrey.php

---
+++ joffrey.php
@@ -0,0 +1,16 @@
+<h1><<<<<< Joffrey >>>>></h1>
+
+<p>This is where youll find all the big-time action, major storylines and iconic Spider-Man magic youd come to expect
+    from the Wall-Crawler. Meet all of Spideys deadly enemies, from the Green Goblin and Doctor Octopus to Venom and
+    Carnage, plus see Peter Parker fall in love, face tragedy and triumph, and learn that with great power comes great
+    responsibility. Looking for the one superhero comic you just have to read. Avengers Assemble.</p>
+
+<p> New threats. Looking for the one superhero comic you just have to read. Featuring the work of Kurt Busiek, George
+    Perez and other quintessential Avengers creators. Meet all of Spideys deadly enemies, from the Green Goblin and
+    Doctor Octopus to Venom and Carnage, plus see Peter Parker fall in love, face tragedy and triumph, and learn that
+    with great power comes great responsibility.</p>
+
+<p>   At the dawn of the Heroic Age, the Avengers will assemble once again. Everyones favorite super team presented in
+    true blockbuster fashion. The classic adventures of Spider-Man from the early days up until the 90s. Earths
+    Mightiest Heroes reunite with their biggest guns at the forefront to take on familiar enemies and new threats
+    alike.</p>
